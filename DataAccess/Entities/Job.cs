0783f5d91d5b187aaceddc046c28519d61be0608
unknown <andresch.denes@gmail.com>
2023-03-14 00:44:54 +0100

    DataAccess 1

---
+++ DataAccess/Entities/Job.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccess.Entities
+{
+    public class Job
+    {
+        public int JobId { get; set; }
+        
+        [Required]
+        public string JobTitle { get; set; }
+        
+        [Required]
+        public string CompanyName { get; set; }
+        
+        public string CompanyUrl { get; set;}
+        
+        public string Image { get; set; }
+        
+        [Required]
+        public string ImageAlt { get; set; }
+        
+        [Required]
+        public string From { get; set; }
+        
+        public string To { get; set; }
+        
+        [Required]
+        public string Description { get; set; }
+    }
+}
---
+++ DataAccess/ResumeContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DataAccess.Entities;
+
+namespace DataAccess
+{
+    public class ResumeContext : DbContext
+    {
+        private DbSet<Job> Jobs { get; set; }
+
+        public ResumeContext()
+        { 
+        }
+    }
+}
