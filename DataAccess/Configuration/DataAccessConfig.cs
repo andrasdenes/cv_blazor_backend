cf8bf6d03509c1a46a07839b7965fa9d4557fdd0
Andras Ferenc Denes <andras.denes@evosoft.com>
2023-03-14 12:11:25 +0100

    DataAccess 2

--- DataAccess/Configuration/DataAccessConfig.cs
+++ DataAccess/Configuration/DataAccessConfig.cs
@@ -9,5 +9,8 @@ namespace DataAccess.Configuration
     internal class DataAccessConfig
     {
         public string CosmosDbConnectionString { get; set; }
+        public string AccountEndpoint { get; set; }
+        public string AccountKey { get; set; }
+        public string DatabaseName { get; set; }
     }
 }
--- DataAccess/DIProvider.cs
+++ DataAccess/DIProvider.cs
@@ -1,7 +1,28 @@
-﻿namespace DataAccess
+﻿using System.Reflection;
+using DataAccess.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataAccess
 {
     public class DIProvider
     {
+        public IServiceProvider ServiceProvider { get; init; }
 
+        public DIProvider()
+        {
+            ServiceProvider = SetupServices();
+        }
+
+        protected ServiceProvider SetupServices()
+        { 
+            var collection = new ServiceCollection();
+            
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddUserSecrets(Assembly.GetEntryAssembly()?.GetName().Name);
+
+            collection.Configure<DataAccessConfig>(configurationBuilder.Build());
+            return collection.BuildServiceProvider();
+        }
     }
 }
