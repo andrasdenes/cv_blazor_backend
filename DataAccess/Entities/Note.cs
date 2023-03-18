987fe5e4bc0b39603e236fbe5751bea72b4571ea
unknown <andresch.denes@gmail.com>
2023-03-18 23:43:03 +0100

    remove commented annotations

--- DataAccess/Entities/Note.cs
+++ DataAccess/Entities/Note.cs
@@ -5,8 +5,6 @@ namespace DataAccess.Entities
 {
     public class Note
     {
-        //[Key]
-        //[DatabaseGenerated(DatabaseGeneratedOption.None)]
         public Guid Id { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
