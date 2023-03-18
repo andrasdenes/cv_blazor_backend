7c5d64f379ece5b49043eec1004bcfca4696f3ed
unknown <andresch.denes@gmail.com>
2023-03-18 09:23:39 +0100

    Revert "ragequit at DI"
    This reverts commit 5183da2f226fd5ccbc36956e802ec81335fd11a8.

---
+++ DataAccess/Configuration/DataAccessConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Configuration
+{
+    internal class DataAccessConfig
+    {
+        public string CosmosDbConnectionString { get; set; }
+        public string AccountEndpoint { get; set; }
+        public string AccountKey { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# cv_blazor_backend
