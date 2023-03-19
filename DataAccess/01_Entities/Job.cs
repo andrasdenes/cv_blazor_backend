72f7d92d2aee1537315b01d75690d85e0bd70fe0
unknown <andresch.denes@gmail.com>
2023-03-19 23:39:29 +0100

    add job entity

---
+++ DataAccess/01_Entities/Job.cs
@@ -0,0 +1,17 @@
+﻿namespace DataAccess._01_Entities
+{
+    public class Job
+    {
+        public Guid Id { get; set; }
+        public List<string> TechStack { get; set; }
+        public List<string> Achievements { get; set; }
+        public string JobTitle { get; set; }
+        public string Description { get; set; }
+        public Uri Image { get; set; }
+        public string ImageAlt { get; set; }
+        public Uri CompanyUrl { get; set; }
+        public string CompanyName { get; set;}
+        public string From { get; set; }
+        public string To { get; set; }
+    }
+}
